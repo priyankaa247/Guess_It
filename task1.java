0fd7b9eedbd483974db89ff5887044baa171e663
priyankaa247 <109588827+priyankaa247@users.noreply.github.com>
2024-06-26 23:34:56 +0530

    Update task1.java

--- task1.java
+++ task1.java
@@ -2,39 +2,40 @@ import java.util.Scanner;
 
 public class NumberGame {
     public static void main(String args[]){
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         int chance=5;
         int finals=0;
         boolean playAgain=true;
-        System.out.println(x:"Welcome to the game !");
-        System.out.rintln("You have about " + chance + "chances to win this game.");
+        System.out.println("Welcome to the game !");
+        System.out.println("You have about " + chance + " chances to win this game.");
         while(playAgain) {
-            int rand= getrandN(min:1,max:100);
+            int rand= getrandN(1,100);
             boolean guess=false;
             for (int i=0; i<chance ; i++){
-                System.out.println("Chance" + (i+1) + " Enter your guess:");
+                System.out.println("Chance " + (i + 1) + " Enter your guess:");
                 int user = sc.nextInt();
-                if(user==rand){
+                if(user == rand){
                     guess=true;
                     finals+=1;
-                    System.out.println(x:"You WON the game.");
+                    System.out.println("You WON the game.");
                     break;
                 }
                 else if(user>rand){
-                    System.out.println(x:"Too High");
+                    System.out.println("Too High");
                 }
                 else{
-                    System.out.println(x:"Too Low");
+                    System.out.println("Too Low");
                 }
             }
-            if(guess==false){
+            if(!guess){
                 System.out.println("Your chances are over , the number is " + rand);
             }
-            System.out.println(x:"Do you want to play again(y/n)?");
+            System.out.println("Do you want to play again(y/n)?");
             String again=sc.next();
-            playAgain=again.equalsIgnoreCase(anotherString:"y");
+            playAgain=again.equalsIgnoreCase("y");
         }
-        System.out.println(x:"Here is your score: " + finals);
+        System.out.println("Here is your score: " + finals);
+        sc.close;
         
     }
     public static int getrandN(int min,int max){
